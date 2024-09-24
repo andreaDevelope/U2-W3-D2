77cf46ceefe21c5459e2002935b6c3fa1edf383e
andreaDevelope <andrea_develope@outlook.it>
2024-09-24 15:39:00 +0200

    refactoring

--- script.js
+++ script.js
@@ -4,23 +4,32 @@ const form = document.querySelector("form");
 const p = document.querySelector("p");
 
 const localStorageKey = "nome";
-const arrNomi = [];
+
+let localArr = localStorage.getItem(localStorageKey) ? JSON.parse(localStorage.getItem(localStorageKey)) : [];
+
+if (localArr.length > 0) {
+  let lastNameIndex = localArr.length - 1;
+  p.innerHTML = `${localArr[lastNameIndex]}`;
+}
 
 const saveFnc = () => {
   const nome = document.getElementById("nome").value;
+
   if (nome.length > 2) {
-    console.log(nome);
-    p.innerHTML = `
-      ${nome}
-      `;
-    arrNomi.push(nome);
-    localStorage.setItem(localStorageKey, JSON.stringify(arrNomi));
+    localArr.push(nome);
+
+    localStorage.setItem(localStorageKey, JSON.stringify(localArr));
+
+    let lastNameIndex = localArr.length - 1;
+    p.innerHTML = `${localArr[lastNameIndex]}`;
+
     form.reset();
   }
 };
 
 const removeFnc = () => {
   localStorage.removeItem(localStorageKey);
+  localArr = [];
   p.innerText = "";
 };
 
